a19bea682776abfac263e75772f4aaafdee406a9
andre <andre.f.alves@tecnico.ulisboa.pt>
2023-12-04 11:50:42 +0000

    Decision trees

---
+++ dataset_health/modeling/DecisionTress.py
@@ -0,0 +1,107 @@
+from typing import Literal
+from numpy import array, ndarray
+from matplotlib.pyplot import figure, savefig, show
+from sklearn.tree import DecisionTreeClassifier
+from config.dslabs_functions import CLASS_EVAL_METRICS, DELTA_IMPROVE, read_train_test_from_files
+from config.dslabs_functions import plot_evaluation_results, plot_multiline_chart, trees_study
+from numpy import argsort
+from config.dslabs_functions import plot_horizontal_bar_chart
+file_tag = "covid"
+train_filename = "dataset_health/data/covid_under.csv"
+test_filename = "dataset_health/data/test.csv"
+target = "CovidPos"
+eval_metric = "f1"
+
+
+trnX, tstX, trnY, tstY, labels, vars = read_train_test_from_files(
+    train_filename, test_filename, target
+)
+
+figure()
+best_model, params = trees_study(trnX, trnY, tstX, tstY, d_max=25, metric=eval_metric)
+savefig(f'dataset_health/modeling/DecisionTree_image/{file_tag}_dt_{eval_metric}_study.png')
+show()
+
+prd_trn: array = best_model.predict(trnX)
+prd_tst: array = best_model.predict(tstX)
+figure()
+plot_evaluation_results(params, trnY, prd_trn, tstY, prd_tst, labels)
+savefig(f'dataset_health/modeling/DecisionTree_image/{file_tag}_dt_{params["name"]}_best_{params["metric"]}_eval.png')
+show()
+
+from sklearn.tree import export_graphviz
+from matplotlib.pyplot import imread, imshow, axis
+from subprocess import call
+
+tree_filename: str = f"dataset_health/modeling/DecisionTree_image/{file_tag}_dt_{eval_metric}_best_tree"
+max_depth2show = 3
+st_labels: list[str] = [str(value) for value in labels]
+
+dot_data: str = export_graphviz(
+    best_model,
+    out_file=tree_filename + ".dot",
+    max_depth=max_depth2show,
+    feature_names=vars,
+    class_names=st_labels,
+    filled=True,
+    rounded=True,
+    impurity=False,
+    special_characters=True,
+    precision=2,
+)
+# Convert to png
+call(
+    ["dot", "-Tpng", tree_filename + ".dot", "-o", tree_filename + ".png", "-Gdpi=600"]
+)
+
+figure(figsize=(14, 6))
+imshow(imread(tree_filename + ".png"))
+axis("off")
+show()
+
+crit: Literal["entropy", "gini"] = params["params"][0]
+d_max = 25
+depths: list[int] = [i for i in range(2, d_max + 1, 1)]
+y_tst_values: list[float] = []
+y_trn_values: list[float] = []
+acc_metric = "accuracy"
+for d in depths:
+    clf = DecisionTreeClassifier(max_depth=d, criterion=crit, min_impurity_decrease=0)
+    clf.fit(trnX, trnY)
+    prd_tst_Y: array = clf.predict(tstX)
+    prd_trn_Y: array = clf.predict(trnX)
+    y_tst_values.append(CLASS_EVAL_METRICS[acc_metric](tstY, prd_tst_Y))
+    y_trn_values.append(CLASS_EVAL_METRICS[acc_metric](trnY, prd_trn_Y))
+
+figure()
+plot_multiline_chart(
+    depths,
+    {"Train": y_trn_values, "Test": y_tst_values},
+    title=f"DT overfitting study for {crit}",
+    xlabel="max_depth",
+    ylabel=str(eval_metric),
+    percentage=True,
+)
+savefig(f"dataset_health/modeling/DecisionTree_image/{file_tag}_dt_{eval_metric}_overfitting.png")
+
+
+
+importances = best_model.feature_importances_
+indices: list[int] = argsort(importances)[::-1]
+elems: list[str] = []
+imp_values: list[float] = []
+for f in range(len(vars)):
+    elems += [vars[indices[f]]]
+    imp_values += [importances[indices[f]]]
+    print(f"{f+1}. {elems[f]} ({importances[indices[f]]})")
+
+figure(figsize=(8,15))
+plot_horizontal_bar_chart(
+    elems,
+    imp_values,
+    title="Decision Tree variables importance",
+    xlabel="importance",
+    ylabel="variables",
+    percentage=True,
+)
+savefig(f"dataset_health/modeling/DecisionTree_image/{file_tag}_dt_{eval_metric}_vars_ranking.png")
--- dataset_health/modeling/config/dslabs_functions.py
+++ dataset_health/modeling/config/dslabs_functions.py
@@ -20,6 +20,9 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_a
 from sklearn.metrics import confusion_matrix, RocCurveDisplay
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from typing import Literal
+from numpy import ndarray, array
 
 from config.config import ACTIVE_COLORS, LINE_COLOR, FILL_COLOR, cmap_blues
 
@@ -447,6 +450,36 @@ def knn_study(trnX, trnY, tstX, tstY, k_max=19, lag=2, metric='accuracy', file_t
 
     plot_multiline_chart(kvalues, values, title=f'KNN Models ({metric})', xlabel='k', ylabel=metric, percentage=True)
     #savefig(f'images/{file_tag}_knn_{metric}_study.png')
+
+    return best_model, best_params
+
+def trees_study(
+    trnX: ndarray, trnY: array, tstX: ndarray, tstY: array, d_max: int=10, lag:int=2, metric='accuracy'
+    ) -> tuple:
+    criteria: list[Literal['entropy', 'gini']] = ['entropy', 'gini']
+    depths: list[int] = [i for i in range(2, d_max+1, lag)]
+
+    best_model: DecisionTreeClassifier | None = None
+    best_params: dict = {'name': 'DT', 'metric': metric, 'params': ()}
+    best_performance: float = 0.0
+
+    values: dict = {}
+    for c in criteria:
+        y_tst_values: list[float] = []
+        for d in depths:
+            clf = DecisionTreeClassifier(max_depth=d, criterion=c, min_impurity_decrease=0)
+            clf.fit(trnX, trnY)
+            prdY: array = clf.predict(tstX)
+            eval: float = CLASS_EVAL_METRICS[metric](tstY, prdY)
+            y_tst_values.append(eval)
+            if eval - best_performance > DELTA_IMPROVE:
+                best_performance = eval
+                best_params['params'] = (c, d)
+                best_model = clf
+            # print(f'DT {c} and d={d}')
+        values[c] = y_tst_values
+    #print(f'DT best with {best_params['params'][0]} and d={best_params['params'][1]}')
+    plot_multiline_chart(depths, values, title=f'DT Models ({metric})', xlabel='d', ylabel=metric, percentage=True)
 
     return best_model, best_params
 
