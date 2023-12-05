70701e3327e369436024e226f00232c07815f861
João Ferreira <joaofferreira4@gmail.com>
2023-12-05 20:49:21 +0000

    Profilling complete refactor

--- dataset_health/profiling/sparsity/scatter.py
+++ dataset_health/profiling/sparsity/scatter.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('.')
+sys.path.append('config')
 from numpy import ndarray
 from pandas import read_csv, DataFrame
 from matplotlib.figure import Figure
@@ -11,6 +14,33 @@ data = data.dropna()
 
 variables_types: dict[str, list] = get_variable_types(data)
 vars = variables_types["numeric"] +  variables_types["symbolic"]
+
+line = 0
+row = 0
+if [] != vars:
+
+    n: int = (len(vars) - 1) 
+    rows = n // 2 + 1
+
+    fig: Figure
+    axs: ndarray
+    fig, axs = subplots(rows, n, figsize=(n * HEIGHT, rows * HEIGHT), squeeze=False)
+    for i in range(len(vars)):
+        var1: str = vars[i]
+        for j in range(i + 1, len(vars)):
+            var2: str = vars[j]
+
+            plot_multi_scatters_chart(data, var1, var2, ax=axs[line, row])
+            row += 1
+            if row == n:
+                row = 0
+                line += 1
+
+    savefig(f"dataset_health/profiling/sparsity/images/covid_scatter.png")
+    # show()
+else:
+    print("Sparsity class: there are no variables.")
+
 if [] != vars:
     target = "stroke"
 
@@ -23,7 +53,7 @@ if [] != vars:
         for j in range(i + 1, len(vars)):
             var2: str = vars[j]
             plot_multi_scatters_chart(data, var1, var2, ax=axs[i, j - 1])
-    savefig(f"dataset_health/profiling/images/covid_scatter")
-    show()
+    savefig(f"dataset_health/profiling/sparsity/images/covid_original_scatter.png")
+    # show()
 else:
     print("Sparsity class: there are no variables.")
