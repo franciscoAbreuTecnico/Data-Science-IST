7af9f89c8d88a7e544ee6933ef66d73af96b812f
andre <andre.f.alves@tecnico.ulisboa.pt>
2023-12-31 10:56:49 +0000

    lstms done

---
+++ forecasting_health/forecasting/lstms.py
@@ -0,0 +1,178 @@
+from torch import no_grad, tensor
+from torch.nn import LSTM, Linear, Module, MSELoss
+from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import StandardScaler
+
+
+def scale_all_dataframe(data):
+    vars: list[str] = data.columns.to_list()
+    transf: StandardScaler = StandardScaler().fit(data)
+    df = DataFrame(transf.transform(data), index=data.index)
+    df.columns = vars
+    return df
+
+
+def prepare_dataset_for_lstm(series, seq_length: int = 4):
+    setX: list = []
+    setY: list = []
+    for i in range(len(series) - seq_length):
+        past = series[i : i + seq_length]
+        future = series[i + 1 : i + seq_length + 1]
+        setX.append(past)
+        setY.append(future)
+    return tensor(setX), tensor(setY)
+
+
+class DS_LSTM(Module):
+    def __init__(self, train, input_size: int = 1, hidden_size: int = 50, num_layers: int = 1, length: int = 4):
+        super().__init__()
+        self.lstm = LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.linear = Linear(hidden_size, 1)
+        self.optimizer = Adam(self.parameters())
+        self.loss_fn = MSELoss()
+
+        trnX, trnY = prepare_dataset_for_lstm(train, seq_length=length)
+        self.loader = DataLoader(TensorDataset(trnX, trnY), shuffle=True, batch_size=len(train) // 10)
+
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.linear(x)
+        return x
+
+    def fit(self):
+        self.train()
+        for batchX, batchY in self.loader:
+            y_pred = self(batchX)
+            loss = self.loss_fn(y_pred, batchY)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+        return loss
+
+    def predict(self, X):
+        with no_grad():
+            y_pred = self(X)
+        return y_pred[:, -1, :]
+    
+
+from pandas import read_csv, DataFrame, Series
+from config.dslabs_functions import series_train_test_split
+
+filename: str = "forecasting_health/data/forecast_covid_single.csv"
+file_tag: str = "covid"
+target: str = "deaths"
+timecol: str = "date"
+
+measure: str = "R2"
+
+data: DataFrame = read_csv(filename, index_col=timecol, sep=",", decimal=".", parse_dates=True)
+data = data.diff().diff()
+data.iloc[0] = 0
+data.iloc[1] = 0
+data = scale_all_dataframe(data)
+
+series = data[[target]].values.astype("float32")
+
+train_size = int(len(series) * 0.90)
+train, test = series[:train_size], series[train_size:]
+
+
+model = DS_LSTM(train, input_size=1, hidden_size=50, num_layers=1)
+loss = model.fit()
+print(loss)
+
+
+from config.dslabs_functions import HEIGHT, plot_multiline_chart
+from copy import deepcopy
+
+from matplotlib.pyplot import figure, savefig, subplots
+from config.dslabs_functions import FORECAST_MEASURES, DELTA_IMPROVE, plot_multiline_chart
+
+
+def lstm_study(train, test, nr_episodes: int = 1000, measure: str = "R2"):
+    sequence_size = [2, 4, 8]
+    nr_hidden_units = [25, 50, 100]
+
+    step: int = nr_episodes // 10
+    episodes = [1] + list(range(0, nr_episodes + 1, step))[1:]
+    flag = measure == "R2" or measure == "MAPE"
+    best_model = None
+    best_params: dict = {"name": "LSTM", "metric": measure, "params": ()}
+    best_performance: float = -100000
+
+    _, axs = subplots(1, len(sequence_size), figsize=(len(sequence_size) * HEIGHT, HEIGHT))
+
+    for i in range(len(sequence_size)):
+        length = sequence_size[i]
+        tstX, tstY = prepare_dataset_for_lstm(test, seq_length=length)
+
+        values = {}
+        for hidden in nr_hidden_units:
+            yvalues = []
+            model = DS_LSTM(train, hidden_size=hidden)
+            for n in range(0, nr_episodes + 1):
+                model.fit()
+                if n % step == 0:
+                    prd_tst = model.predict(tstX)
+                    eval: float = FORECAST_MEASURES[measure](test[length:], prd_tst)
+                    print(f"seq length={length} hidden_units={hidden} nr_episodes={n}", eval)
+                    if eval > best_performance and abs(eval - best_performance) > DELTA_IMPROVE:
+                        best_performance: float = eval
+                        best_params["params"] = (length, hidden, n)
+                        best_model = deepcopy(model)
+                    yvalues.append(eval)
+            values[hidden] = yvalues
+        plot_multiline_chart(
+            episodes,
+            values,
+            ax=axs[i],
+            title=f"LSTM seq length={length} ({measure})",
+            xlabel="nr episodes",
+            ylabel=measure,
+            percentage=flag,
+        )
+    print(
+        f"LSTM best results achieved with length={best_params["params"][0]} hidden_units={best_params["params"][1]} and nr_episodes={best_params["params"][2]}) ==> measure={best_performance:.2f}"
+    )
+    return best_model, best_params
+
+
+best_model, best_params = lstm_study(train, test, nr_episodes=3000, measure=measure)
+
+
+from config.dslabs_functions import plot_forecasting_eval
+
+params = best_params["params"]
+best_length = params[0]
+trnX, trnY = prepare_dataset_for_lstm(train, seq_length=best_length)
+tstX, tstY = prepare_dataset_for_lstm(test, seq_length=best_length)
+
+prd_trn = best_model.predict(trnX)
+prd_tst = best_model.predict(tstX)
+
+plot_forecasting_eval(
+    train[best_length:],
+    test[best_length:],
+    prd_trn,
+    prd_tst,
+    title=f"{file_tag} - LSTM (length={best_length}, hidden={params[1]}, epochs={params[2]})",
+)
+savefig(f"forecasting_health/forecasting/images/{file_tag}_lstms_{measure}_eval.png")
+
+
+from config.dslabs_functions import plot_forecasting_series
+
+series = data[[target]]
+train, test = series[:train_size], series[train_size:]
+pred_series: Series = Series(prd_tst.numpy().ravel(), index=test.index[best_length:])
+
+plot_forecasting_series(
+    train[best_length:],
+    test[best_length:],
+    pred_series,
+    title=f"{file_tag} - LSTMs ",
+    xlabel=timecol,
+    ylabel=target,
+)
+savefig(f"forecasting_health/forecasting/images/{file_tag}_lstms_{measure}_forecast.png")
